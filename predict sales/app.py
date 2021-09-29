5a2b2d0763dba1c165cdd027bee20a20bd60e50a
sohom mandal <sohomiitb@gmail.com>
2021-09-29 15:22:43 -0400

    change dataset

--- predict sales/app.py
+++ predict sales/app.py
@@ -18,7 +18,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Sales should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Loan should be $ {}'.format(output))
 
 @app.route('/results',methods=['POST'])
 def results():
